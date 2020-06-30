142657659b9e3785ba6347c9e2ce20e6bd773b4f
WakkoStar <46996066+WakkoStar@users.noreply.github.com>
2020-06-30 19:36:27 +0200

    Updates

---
+++ precache-manifest.419bcdc977e4e33c2d64a8150ac4c01c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423ea40f5c8f362ab39c4fc3d6105fdb",
+    "url": "/vers-de-ter-front/index.html"
+  },
+  {
+    "revision": "38ab1fcfe6a6d025b3b5",
+    "url": "/vers-de-ter-front/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10354a6d2cf02dfff891",
+    "url": "/vers-de-ter-front/static/css/main.e492cc87.chunk.css"
+  },
+  {
+    "revision": "38ab1fcfe6a6d025b3b5",
+    "url": "/vers-de-ter-front/static/js/2.5f79261c.chunk.js"
+  },
+  {
+    "revision": "ce12de340ef4804f0d244ce30d144354",
+    "url": "/vers-de-ter-front/static/js/2.5f79261c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10354a6d2cf02dfff891",
+    "url": "/vers-de-ter-front/static/js/main.9d63201e.chunk.js"
+  },
+  {
+    "revision": "47afc9e27d537a737ee4",
+    "url": "/vers-de-ter-front/static/js/runtime-main.a0d77c1d.js"
+  }
+]);
